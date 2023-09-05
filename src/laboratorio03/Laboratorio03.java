87b0215146ac7d823a32be7c425b975defcb59cb
FabrizioGrosso16 <142623045+FabrizioGrosso16@users.noreply.github.com>
2023-09-04 21:18:47 -0500

    Add files via upload

---
+++ src/laboratorio03/Laboratorio03.java
@@ -0,0 +1,17 @@
+package laboratorio03;
+
+/**
+ *
+ * @author cs288
+ */
+public class Laboratorio03 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        ListaPersona listaPersona = new ListaPersona();
+        listaPersona.setVisible(true);
+    }
+    
+}
